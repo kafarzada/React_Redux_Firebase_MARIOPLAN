2fe8690da48148d9d7a6ed14815153d7dc5d5960
Kafarzada Vurgun <kafarzada@gmail.com>
2021-02-14 04:49:24 +0700

    добвил тестовые данные в редюссер

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const ProjectSummary = () => {
+const ProjectSummary = (props) => {
     return (
     <div className="card z-depth-0 project-summary">
         <div className="card-content grey-text text-darken-3">
-            <span className="card-title">Project Title</span>
-            <p>Post by the Net Ninjs</p>
+            <span className="card-title">{props.project.title}</span>
+            <p>{props.project.content}</p>
             <p className="grey-text">3rd Septemper, 2em</p>
         </div>
     </div>
--- src/store/redusers/projectReducer.js
+++ src/store/redusers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState = {}
+const initState = {
+    projects: [
+        {id: "1", title: "title", content: "some content"},
+        {id: "2", title: "title", content: "some content"},
+        {id: "3", title: "title", content: "some content"}
+    ]
+}
 
 const ProjectReducer = (state = initState, action) => {
     return state

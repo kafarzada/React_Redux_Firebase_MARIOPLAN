e055e1b422d50a635f04b089769a33ae184587da
Kafarzada Vurgun <kafarzada@gmail.com>
2021-02-14 15:51:57 +0700

    добавил actions

--- src/store/redusers/projectReducer.js
+++ src/store/redusers/projectReducer.js
@@ -1,13 +1,23 @@
 const initState = {
     projects: [
-        {id: "1", title: "title", content: "some content"},
-        {id: "2", title: "title", content: "some content"},
-        {id: "3", title: "title", content: "some content"}
+        {id: "1", title: "title 1", content: "some content"},
+        {id: "2", title: "title 2", content: "some content"},
+        {id: "3", title: "title 3", content: "some content"}
     ]
 }
 
 const ProjectReducer = (state = initState, action) => {
-    return state
+    switch (action.type) {
+        case "CREATE_PROJECT":
+            console.log("Create a new project", action.project);
+            return {
+                ...state,
+                projects: [...state.projects, action.project]
+            }
+    
+        default:
+            return state
+    }
 }
 
 export default ProjectReducer
